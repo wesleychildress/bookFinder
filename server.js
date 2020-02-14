067d8e0ed60baf2ca02ee21a2a7df22a2ec31edc
WesleyChildress <wesleychildress@icloud.com>
2020-02-14 09:58:27 -0500

    deploy to heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/',(req, res) => {
     res.render('home');
